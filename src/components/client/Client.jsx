c0ef8d0451cb32a38c99b34629b28f0cb35a81ff
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-06-09 20:01:43 -0300

    use api

--- src/components/client/Client.jsx
+++ src/components/client/Client.jsx
@@ -1,69 +1,73 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
 import NavBarLanding from "../navs/NavBarLanding";
 import Footer from "../footer/Footer";
-import mate2 from "../../assets/mate2.png";
-import termoboca from "../../assets/termo.png";
-import termocac from "../../assets/termocac.jpg";
-import termocentrall from "../../assets/termocentrall.png";
+// import mate2 from "../../assets/mate2.png";
+// import termoboca from "../../assets/termo.png";
+// import termocac from "../../assets/termocac.jpg";
+// import termocentrall from "../../assets/termocentrall.png";
 import { Card } from "react-bootstrap";
 
-const products = [
-  {
-    id: 1,
-    category: "Mates",
-    name: "Mate Imperial",
-    price: 40000,
-    image: mate2,
-  },
-  {
-    id: 2,
-    category: "Termos",
-    name: "Termo Stanley",
-    price: 40000,
-    image: termocac,
-  },
-  {
-    id: 3,
-    category: "Mochilas",
-    name: "Mochila Matera",
-    price: 40000,
-    image: termoboca,
-  },
-  {
-    id: 4,
-    category: "Bombillas",
-    name: "Bombilla",
-    price: 40000,
-    image: termocentrall,
-  },
-  {
-    id: 5,
-    category: "Bombillas",
-    name: "Bombilla",
-    price: 40000,
-    image: "img",
-  },
-  {
-    id: 6,
-    category: "Bombillas",
-    name: "Bombilla",
-    price: 40000,
-    image: "img",
-  },
-  {
-    id: 7,
-    category: "Bombillas",
-    name: "Bombilla",
-    price: 40000,
-    image: "img",
-  },
-];
-
+// const products = [
+//   {
+//     id: 1,
+//     category: "Mates",
+//     name: "Mate Imperial",
+//     price: 40000,
+//     image: mate2,
+//   },
+//   {
+//     id: 2,
+//     category: "Termos",
+//     name: "Termo Stanley",
+//     price: 40000,
+//     image: termocac,
+//   },
+//   {
+//     id: 3,
+//     category: "Mochilas",
+//     name: "Mochila Matera",
+//     price: 40000,
+//     image: termoboca,
+//   },
+//   {
+//     id: 4,
+//     category: "Bombillas",
+//     name: "Bombilla",
+//     price: 40000,
+//     image: termocentrall,
+//   },
+//   {
+//     id: 5,
+//     category: "Bombillas",
+//     name: "Bombilla",
+//     price: 40000,
+//     image: "img",
+//   },
+//   {
+//     id: 6,
+//     category: "Bombillas",
+//     name: "Bombilla",
+//     price: 40000,
+//     image: "img",
+//   },
+//   {
+//     id: 7,
+//     category: "Bombillas",
+//     name: "Bombilla",
+//     price: 40000,
+//     image: "img",
+//   },
+// ];
+// const filteredProducts = products
+//   .filter((product) =>
+//     product.name.toLowerCase().includes(searchTerm.toLowerCase())
+//   )
+//   .filter((product) => !filter || product.category === filter);
 const Client = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filter, setFilter] = useState("");
-  // const navigate = useNavigate();
+  const [productsFiltered, setProductsFiltered] = useState([]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -73,11 +77,24 @@ const Client = () => {
     setFilter(category);
   };
 
-  const filteredProducts = products
-    .filter((product) =>
-      product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-    .filter((product) => !filter || product.category === filter);
+  useEffect(() => {
+    fetch("http://localhost:8000/products", {
+      headers: {
+        accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((products) => {
+        const filteredProducts = products
+          .filter((product) =>
+            product.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .filter((product) => !filter || product.category === filter);
+
+        setProductsFiltered(filteredProducts);
+      })
+      .catch((error) => console.log(error));
+  }, [searchTerm, filter]);
 
   return (
     <div>
@@ -101,9 +118,9 @@ const Client = () => {
               onChange={handleSearch}
             />
           </div>
-          {filteredProducts.length > 0 ? (
+          {productsFiltered.length > 0 ? (
             <div className="card-container">
-              {filteredProducts.map((product) => (
+              {productsFiltered.map((product) => (
                 <Card key={product.id} className="product-card">
                   <div>
                     <img src={product.image} alt={product.name} />
--- src/services/apiContext/Api.context.jsx
+++ src/services/apiContext/Api.context.jsx
@@ -1,7 +1,25 @@
-// const Api.context = () => {
-//   return (
-//     <div>Api.context</div>
-//   )
-// }
+// import { useState, createContext } from "react";
 
-// export default Api.context
+// export const ApiContext = createContext();
+
+// const userValue = JSON.parse(localStorage.getItem("user"));
+
+// export const ApiContextProvider = ({ children }) => {
+//   const [user, setUser] = useState(userValue);
+
+//   const handleLogout = () => {
+//     localStorage.removeItem("user");
+//     setUser(null);
+//   };
+
+//   const handleLogin = (email) => {
+//     localStorage.setItem("user", JSON.stringify({ email }));
+//     setUser({ email });
+//   };
+
+//   return (
+//     <ApiContext.Provider value={{ user, handleLogin, handleLogout }}>
+//       {children}
+//     </ApiContext.Provider>
+//   );
+// };
