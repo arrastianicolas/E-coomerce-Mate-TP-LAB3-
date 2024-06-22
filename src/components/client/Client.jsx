2c8d8200d1343aad787afc1e8d921e83582b8f6c
Valentino Cueto <valentinocueto2005@gmail.com>
2024-06-22 17:17:30 -0300

    useCallBack and useMemo aggregate

--- src/components/client/Client.jsx
+++ src/components/client/Client.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useMemo, useCallback } from "react";
 import NavBarLanding from "../navs/NavBarLanding";
 import Footer from "../footer/Footer";
 import { Card } from "react-bootstrap";
@@ -6,58 +6,48 @@ import { ApiContext } from "../../services/apiContext/Api.context";
 import SpinnerShops from "../spinnerShops/SpinnerShops";
 
 const Client = () => {
-
-  // Definimos los estados del componente
   const [searchTerm, setSearchTerm] = useState("");
   const [filter, setFilter] = useState("");
-  const [productsFiltered, setProductsFiltered] = useState([]);
   const [loading, setLoading] = useState(true);
   const { products, setCart } = useContext(ApiContext);
 
-  //Definimos funciones
-
-  // Manejar barra de búsqueda
-  const handleSearch = (event) => {
+  const handleSearch = useCallback((event) => {
     setSearchTerm(event.target.value);
-  };
+  }, []);
 
-  // Función para manejar el filtro por categoría
-  const handleFilter = (category) => {
+  const handleFilter = useCallback((category) => {
     setFilter(category);
-  };
+  }, []);
 
-  // Funcion para agregar los productos al carrito
-  const addToCart = (product) => {
-    setCart((prevCart) => {
-      const itemInCart = prevCart.find((item) => item.id === product.id);
-      if (itemInCart) {
-        return prevCart.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      }
-      return [...prevCart, { ...product, quantity: 1 }];
-    });
-  };
+  const addToCart = useCallback(
+    (product) => {
+      setCart((prevCart) => {
+        const itemInCart = prevCart.find((item) => item.id === product.id);
+        if (itemInCart) {
+          return prevCart.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          );
+        }
+        return [...prevCart, { ...product, quantity: 1 }];
+      });
+    },
+    [setCart]
+  );
 
-  // Filtrar productos basado en término de búsqueda y filtro de categoría
-  useEffect(() => {
-    const filteredProducts = products
+  const productsFiltered = useMemo(() => {
+    return products
       .filter(
         (product) =>
           product.name &&
           product.name.toLowerCase().includes(searchTerm.toLowerCase())
       )
       .filter((product) => !filter || product.category === filter);
-
-    setProductsFiltered(filteredProducts);
-    setLoading(false);
   }, [searchTerm, filter, products]);
 
-  // Funcion para manejar el spinner
   useEffect(() => {
-    setLoading(true); 
+    setLoading(true);
     const timer = setTimeout(() => {
       setLoading(false);
     }, 500);
@@ -89,31 +79,27 @@ const Client = () => {
             />
           </div>
 
-          {/* Renderizado del spinner */}
           {loading ? (
             <SpinnerShops />
+          ) : productsFiltered.length > 0 ? (
+            <div className="card-container">
+              {productsFiltered.map((product) => (
+                <Card key={product.id} className="product-card">
+                  <div>
+                    <img src={product.image} alt={product.name} />
+                    <h4>{product.name}</h4>
+                    <p>${product.price}</p>
+                    <button onClick={() => addToCart(product)}>
+                      Agregar al carrito
+                    </button>
+                  </div>
+                </Card>
+              ))}
+            </div>
           ) : (
-            /* Mostramos productos filtrados o mensaje de no encontrado */
-            productsFiltered.length > 0 ? (
-              <div className="card-container">
-                {productsFiltered.map((product) => (
-                  <Card key={product.id} className="product-card">
-                    <div>
-                      <img src={product.image} alt={product.name} />
-                      <h4>{product.name}</h4>
-                      <p>${product.price}</p>
-                      <button onClick={() => addToCart(product)}>
-                        Agregar al carrito
-                      </button>
-                    </div>
-                  </Card>
-                ))}
-              </div>
-            ) : (
-              <h3 className="message-not-product">
-                ¡No se encontraron Productos!
-              </h3>
-            )
+            <h3 className="message-not-product">
+              ¡No se encontraron Productos!
+            </h3>
           )}
         </div>
       </div>
